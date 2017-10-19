cdcf13f7aeadb227cbc1727ce700a3456f2c50a1
admin <jonathan.marcus.chu@gmail.com>
2017-10-19 19:11:18 +1100

    changes

--- server.js
+++ server.js
@@ -3,21 +3,8 @@ const app = express();
 const http = require('http');
 const path = require('path');
 const https = require('https');
-const forceSSL = require('express-force-ssl');
-const helmet = require('helmet');
-const fs = require ('fs');
-
 
 const api = require('./routes/api');
-
-// use helmet
-
-app.use(helmet());
-
-// force SSL
-app.use(forceSSL);
-
-
 
 
 // if create routes api will prefix all routes
@@ -32,7 +19,9 @@ app.get('/', function (req, res) {
     res.render('index', { title: 'Jonathan', message: 'Welcome to Jonathan\'s homepage' })
 })
 
+
+
 const port = process.env.PORT || '3000';
 app.set('port', port);
-const server = https.createServer(app).listen(port);
+const server = http.createServer(app).listen(port);
 server.listen(port, () => console.log(`API running on localhost:${port}`));
