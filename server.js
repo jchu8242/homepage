1a35246665663c8ded2a5291c0d541f2146653d0
admin <jonathan.marcus.chu@gmail.com>
2017-10-19 18:01:26 +1100

    fixing home page

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+const path = require('path');
 
 const api = require('./routes/api');
+
+// if create routes api will prefix all routes
 app.use('/api', api);
+
+// setting pug view engine
 app.set('view engine', 'pug');
 
+// homepage
+
+app.get('/', function (req, res) {
+    res.render('index', { title: 'Jonathan', message: 'Welcome to Jonathan\'s homepage' })
+})
 
 const port = process.env.PORT || '3000';
 app.set('port', port);
